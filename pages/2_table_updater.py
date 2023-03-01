216370dea1d4937a43cc2068a0c0fb7220073a58
sfc-gh-pneedleman <paul.needleman@snowflake.com>
2023-02-28 22:44:29 -0500

    Update 2_table_updater.py

--- pages/2_table_updater.py
+++ pages/2_table_updater.py
@@ -1,11 +1,9 @@
-from jsonschema import Draft202012Validator
 import streamlit as st
 import pandas as pd
-import pydeck as pdk
-from urllib.error import URLError
 import numpy as np
 import snowflake.connector
 import string
+import json
 
 st.set_page_config(page_title="Table Editor", page_icon="🌍", layout="wide")
 
@@ -27,29 +25,53 @@ ctx = snowflake.connector.connect(
 cs = ctx.cursor()
 
 st.write("What would you like to name your table")
-table_nanme = st.text_input('Table Name')
-if table_nanme:
-    st.write("You entered: ", table_nanme)
+table_name = st.text_input('Table Name')
+if table_name:
+    st.write("You entered: ", table_name)
     
-    select_stmt = "SELECT * FROM  " +  table_nanme    
+    select_stmt = "SELECT * FROM  " +  table_name    
 
     cs.execute(select_stmt)
     df = cs.fetch_pandas_all()
 
-    df.set_index("ID", inplace=True)
+    #df.set_index("ID", inplace=True)
 
     edited_df = st.experimental_data_editor(df, key="data_editor", use_container_width=True, num_rows="dynamic") # 👈 Set a key
     st.write("Here's the session state:")
-    st.write(st.session_state["data_editor"]) #
-
-    df.columns[1]
+    st.write(st.session_state["data_editor"])
+    
+    json_raw = st.session_state["data_editor"]
+    
+   
 
     submit =st.button("Submit Changes")
-    #if submit: 
-    #
-    #    sql= "CREATE OR REPLACE TABLE TMP (JSON_DATA VARIANT)"
-    #    cs.execute(sql)
-    #    sql_insert ="INSERT INTO TMP  SELECT PARSE_JSON(TO_JSON("+ str(st.session_state["data_editor"]) + "))"
-    #    st.write(sql_insert)
-    #    cs.execute(sql_insert)
+    if submit: 
+        
+         
+        
+        for key in json_raw:
+            value = json_raw[key]
+            #st.write("The key and value are ({}) = ({})".format(key, value))
+            if key == "deleted_rows":
+                temp_df = pd.DataFrame.from_dict(json_raw['deleted_rows'])
+                temp_df.columns = ['VAL']
+                #st.write(df_new)
+                delete_df = pd.merge(temp_df, df, left_on='VAL', right_index=True)
+                #st.write(delete_df)
+
+                col_list = str(tuple(delete_df['ID'].astype(str).tolist()))
+                delete_stmt = "DELETE FROM " + table_name + " WHERE ID IN " + col_list
+                cs.execute(delete_stmt)
+                st.success('Data Deleted - Rows: ' + col_list)
+
+                
+
+
+
+                
+
+                    
+                    
+                
+
 
